7f8553d73fa186109439f52476bcb7946819236e
Peter <lilv447@gmail.com>
2025-02-12 04:03:57 -0500

    changed gui class to inheret from ctk

--- gui/gui.py
+++ gui/gui.py
@@ -1,34 +1,31 @@
 import customtkinter as ctk
 
 
-class gui:
+class gui(ctk.CTk):
 
-    def __init__(self, root):
-        self.root = root
-        self.root.title = 'Weather'
-        self.root.geometry('400x300')
+    def __init__(self, get_weather):
+        super().__init__()
+
+        self.title = 'Weather'
+        self.geometry('400x300')
 
         ctk.set_appearance_mode('dark')
         ctk.set_default_color_theme('blue')
 
-        self.label = ctk.CTkLabel(root, text='Enter location', font=('Arial', 14))
+        self.label = ctk.CTkLabel(self, text='Enter location', font=('Arial', 14))
         self.label.pack(pady=10)
 
-        self.entry = ctk.CTkEntry(root, font=('Arial', 14))
+        self.entry = ctk.CTkEntry(self, font=('Arial', 14))
         self.entry.pack(pady=5)
 
-        self.weather_label = ctk.CTkLabel(root, text='', font=('Arial', 14))
+        self.weather_label = ctk.CTkLabel(self, text='', font=('Arial', 14))
         self.weather_label.pack(pady=10)
 
-        self.button = ctk.CTkButton(root, text='Get Weather', command=self.get_weather, font=('Arial', 14))
+        self.button = ctk.CTkButton(self, text='Get Weather', command=get_weather, font=('Arial', 14))
         self.button.pack(pady=10)
 
-    def get_weather(self):
-        city = self.entry.get()
-        self.weather_label.configure(text=f'Weather in {city} is fucking beautiful bruv')   # Test as placeholder
 
 if __name__ == '__main__':
-    root = ctk.CTk()
-    app = gui(root)
-    root.mainloop()
+    app = gui()
+    app.mainloop()
 
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from weather.get_weather import getWeather
+from gui.gui import gui
+
+
 def main():
-    pass
+    #TODO fix this by creating a wrapper function in gui class and store a reference to get_weather rather than calling it right here where it expects a param
+    location = input('Enter location >> ')
+    weather = getWeather()
+    app = gui(weather.get_weather(location))
+    app.mainloop()
 
 
 if __name__ == '__main__':
