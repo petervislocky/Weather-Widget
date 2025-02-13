863f29784493084b0dabc95f80dc27d1c4c3d1c6
Peter Vislocky <lilv447@gmail.com>
2025-02-13 00:59:00 -0500

    created functioning text box that returns weather data below for given area

--- gui/gui.py
+++ gui/gui.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from weather.get_weather import getWeather
 
 
 class gui(ctk.CTk):
@@ -6,10 +7,12 @@ class gui(ctk.CTk):
     def __init__(self, get_weather):
         super().__init__()
 
-        self.title = 'Weather'
-        self.geometry('400x300')
+        self.get_weather = get_weather
 
-        ctk.set_appearance_mode('dark')
+        self.title('Weather')
+        self.geometry('350x250')
+
+        ctk.set_appearance_mode('system')
         ctk.set_default_color_theme('blue')
 
         self.label = ctk.CTkLabel(self, text='Enter location', font=('Arial', 14))
@@ -21,8 +24,26 @@ class gui(ctk.CTk):
         self.weather_label = ctk.CTkLabel(self, text='', font=('Arial', 14))
         self.weather_label.pack(pady=10)
 
-        self.button = ctk.CTkButton(self, text='Get Weather', command=get_weather, font=('Arial', 14))
+        self.button = ctk.CTkButton(self, text='Get Weather', command=self.pass_location, font=('Arial', 14))
         self.button.pack(pady=10)
+
+    def pass_location(self):
+        '''
+        Wrapper function for get_weather after a reference to it is stored in the class
+        '''
+        gw = getWeather()
+        location = self.entry.get()
+        if location:
+            api_repsonse = self.get_weather(location)
+            name, region, country, text, icon, temp_f, feelslike_f, wind_mph, temp_c, feelslike_c, wind_kph = gw.parse_weather(api_repsonse)
+            if api_repsonse:
+                formatted_weather = f'Temp: {temp_f}\N{DEGREE SIGN}F\nFeels like: {feelslike_f}\N{DEGREE SIGN}F\nCondition: {text}'
+                self.weather_label.configure(text=formatted_weather)
+            else:
+                self.weather_label.configure('Error no proper data to display')
+        else:
+            self.weather_label.configure('No location given')
+        
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from gui.gui import gui
 
 
 def main():
-    #TODO fix this by creating a wrapper function in gui class and store a reference to get_weather rather than calling it right here where it expects a param
-    location = input('Enter location >> ')
     weather = getWeather()
-    app = gui(weather.get_weather(location))
+    app = gui(weather.get_weather)
     app.mainloop()
 
 
